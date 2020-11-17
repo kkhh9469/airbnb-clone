476ea42342139b60963feea00af9695fc149097a
kkhh9469 <66230761+kkhh9469@users.noreply.github.com>
2020-11-17 11:31:12 +0900

    update django_secret

--- config/settings.py
+++ config/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "y_ng&70n#&c591lci%3iiaeo-otxo0$#&2f=w=u)o9anuz&d=&"
+SECRET_KEY = os.environ.get("DJANGO_SECRET")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get("DEBUG"))
