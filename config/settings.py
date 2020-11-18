80cc9e7ee85d610701d5ccfd3aa0b58ce8328f7b
kkhh9469 <66230761+kkhh9469@users.noreply.github.com>
2020-11-18 23:55:44 +0900

    up

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".airbnb-clone-test.com"]
+ALLOWED_HOSTS = ["airbnb-clone-test.eba-zypek5yp.ap-northeast-2.elasticbeanstalk.com"]
 
 
 # Application definition
