ded9083e6071a8a93617bb4afec43a17acc101fb
kkhh9469 <66230761+kkhh9469@users.noreply.github.com>
2020-11-19 00:13:30 +0900

    up

--- config/settings.py
+++ config/settings.py
@@ -91,7 +91,7 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-if DEBUG is False:
+if DEBUG:
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.sqlite3",
