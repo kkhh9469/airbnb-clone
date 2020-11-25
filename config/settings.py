31298a5ba23d7d5ff4adecc836dc1b3ae9b32f85
kkhh9469 <66230761+kkhh9469@users.noreply.github.com>
2020-11-26 00:38:54 +0900

    update

--- config/settings.py
+++ config/settings.py
@@ -182,7 +182,7 @@ if not DEBUG:
     # sentry
 
     AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.ap-northeast-2.amazonaws.com"
-    STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static"
+    STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static/"
 
     sentry_sdk.init(
         dsn=os.environ.get("SENTRY_URL"),
