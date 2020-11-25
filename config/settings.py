48c19ef97584adfc4121cc8605afac20a8e1cd5e
kkhh9469 <66230761+kkhh9469@users.noreply.github.com>
2020-11-26 01:12:46 +0900

    update

--- config/settings.py
+++ config/settings.py
@@ -170,19 +170,19 @@ LOGIN_URL = "/users/login"
 
 LOCALE_PATHS = (os.path.join(BASE_DIR, "locale"),)
 
-DEFAULT_FILE_STORAGE = "config.custom_storages.UploadStorage"
-STATICFILES_STORAGE = "config.custom_storages.StaticStorage"
-AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
-AWS_STORAGE_BUCKET_NAME = "airbnb-clone-kkhh9469"
-AWS_DEFAULT_ACL = "public-read"
-
 if not DEBUG:
 
-    # sentry
+    DEFAULT_FILE_STORAGE = "config.custom_storages.UploadStorage"
+    STATICFILES_STORAGE = "config.custom_storages.StaticStorage"
+    AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+    AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+    AWS_STORAGE_BUCKET_NAME = "airbnb-clone-kkhh9469"
+    AWS_DEFAULT_ACL = "public-read"
 
     AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.ap-northeast-2.amazonaws.com"
     STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static/"
+
+    # sentry
 
     sentry_sdk.init(
         dsn=os.environ.get("SENTRY_URL"),
