eb87c3ff45f030ce2858a2ec4e512d12eb875fa9
kkhh9469 <66230761+kkhh9469@users.noreply.github.com>
2020-11-25 23:03:23 +0900

    update

--- config/settings.py
+++ config/settings.py
@@ -41,7 +41,7 @@ DJANGO_APPS = [
     "django.contrib.staticfiles",
 ]
 
-THIRD_PARTY_APPS = ["django_countries", "django_seed"]
+THIRD_PARTY_APPS = ["django_countries", "django_seed", "storages"]
 
 PROJECT_APPS = [
     "core.apps.CoreConfig",
@@ -169,10 +169,17 @@ LOGIN_URL = "/users/login"
 
 LOCALE_PATHS = (os.path.join(BASE_DIR, "locale"),)
 
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = "airbnb-clone-kkhh9469"
 
-# sentry
 
 if not DEBUG:
+
+    # sentry
+
     sentry_sdk.init(
         dsn=os.environ.get("SENTRY_URL"),
         integrations=[DjangoIntegration()],
