2c3ccb979035def1f6e6a07c3470f0b0636ae783
kkhh9469 <66230761+kkhh9469@users.noreply.github.com>
2020-11-27 21:45:06 +0900

    change kakao login

--- users/views.py
+++ users/views.py
@@ -144,7 +144,7 @@ def github_callback(request):
 
 def kakao_login(request):
     client_id = os.environ.get("KAKAO_ID")
-    redirect_uri = "http://127.0.0.1:8000/users/login/kakao/callback"
+    redirect_uri = "http://airbnb-clone-test.eba-zypek5yp.ap-northeast-2.elasticbeanstalk.com/users/login/kakao/callback"
     return redirect(
         f"https://kauth.kakao.com/oauth/authorize?client_id={client_id}&redirect_uri={redirect_uri}&response_type=code"
     )
