cf9b7ff41af91692819f3ef56bb16cf7e42b8c22
kkhh9469 <66230761+kkhh9469@users.noreply.github.com>
2020-11-27 22:41:55 +0900

    update rooms

--- rooms/urls.py
+++ rooms/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = "rooms"
 
 urlpatterns = [
-    path("reate/", views.CreateRoomView.as_view(), name="create"),
+    path("create/", views.CreateRoomView.as_view(), name="create"),
     path("<int:pk>/", views.RoomDetail.as_view(), name="detail"),
     path("<int:pk>/edit/", views.EditRoomView.as_view(), name="edit"),
     path("<int:pk>/photos/", views.RoomPhotosView.as_view(), name="photos"),
