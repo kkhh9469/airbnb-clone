dd799edc8952b76fb051575b0f04c8c552f44809
kkhh9469 <66230761+kkhh9469@users.noreply.github.com>
2020-11-27 21:33:31 +0900

    change_admin

--- config/urls.py
+++ config/urls.py
@@ -31,7 +31,7 @@ urlpatterns = [
     path("reviews/", include("reviews.urls", namespace="reviews")),
     path("lists/", include("lists.urls", namespace="lists")),
     path("conversations/", include("conversations.urls", namespace="conversations")),
-    path("admin/", admin.site.urls),
+    path("eb-admin/", admin.site.urls),
     path("sentry-debug/", trigger_error),
 ]
 
